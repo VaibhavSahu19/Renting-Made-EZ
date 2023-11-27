6cd0f4a9fa351044396378e653761ecbd88993de
VaibhavSahu19 <vaibhavsahu200@gmail.com>
2023-11-27 10:58:38 +0530

    initial commit

---
+++ Frontend/index.js
@@ -0,0 +1,2 @@
+import './Pages/index.html'; // Import your HTML file
+import './Pages/landingpage.css'; // Import your CSS file
---
+++ Frontend/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './Pages/index.html', // Path to your HTML template
+      filename: 'index.html', // Name of the bundled HTML file
+    }),
+  ],
+};
